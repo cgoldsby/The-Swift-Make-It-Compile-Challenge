33f0a4747bcead672f883af53c5a3237058204b8
cgoldsby <cgoldsby@gmail.com>
2017-05-01 17:57:27 -0400

    Challenge 2

--- Make It Compile Challenge.playground/Pages/Challenge #2.xcplaygroundpage/Contents.swift
+++ Make It Compile Challenge.playground/Pages/Challenge #2.xcplaygroundpage/Contents.swift
@@ -3,33 +3,20 @@
 
 
 // Add your code above 👆
-/*: 
-
+/*:
+ 
 #### Make the following code compile
 Don't edit the following code block or wrap it in a comment block /\* *\/ 😉
-
+ 
 ---
-*/
-func raw<T: RawRepresentable>(t: T) -> T.RawValue {
-	return t.rawValue
-}
-
-let array : [E] = [.A(true), .B(1), .C("C")]
-
-array.map(raw)
-
-if case .C(let s?)? = array.first {}
-
-// Bonus:
-
-for elem in array {
-	switch elem {
-	case .C(_):
-		print("C!")
-	}
+ */
+let 🙊: A = nil
+let 🙈: A = [🙊]
+if 🙊 ☠ 🙈, let 🎉 = 🙊[self] {
+    print(self❤️ + 🎉)
 }
 /*:
- ---
+---
  
 After making the previous code block compile, the syntax highlighting (Dusk Theme) should match this picture:
  
